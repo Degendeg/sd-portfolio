dd6b27ecc2834136ec17ad7b81956cd9027778f6
Sebastian Degerman <sebbed89@gmail.com>
2024-03-20 22:30:01 +0100

    added large screen media query, fix css, remove btns in RndFact.jsx

--- src/comps/RndFact.jsx
+++ src/comps/RndFact.jsx
@@ -6,7 +6,6 @@ const RndFact = ({ itemsRef, isDark }) => {
         <div className="card">
           <h3>Work in progress 🚧</h3>
           <p>Löksås ipsum flera om färdväg där som ännu åker gör, sjö sista miljoner hans jäst fram från vidsträckt denna, precis stora se det tid äng olika räv. Stora äng hela mjuka strand ser bland mot, lax och dimmhöljd på kan denna precis, sista vi det bland så tiden.</p>
-          <a href="#">Learn more</a>
         </div>
       </div>
       <div className="item" ref={itemsRef}>
@@ -14,7 +13,6 @@ const RndFact = ({ itemsRef, isDark }) => {
         <div className="card">
           <h3>Work in progress 🚧</h3>
           <p>Vid sitt tidigare annan bland äng vemod plats dimma där rot, genom annat år träutensilierna ta lax se där blivit, kan trevnadens blev enligt därmed själv vid genom kanske. Lax mot dimmhöljd upprätthållande ordningens tid annan sorgliga omfångsrik icke groda.</p>
-          <a href="#">Learn more</a>
         </div>
       </div>
       <div className="item">
@@ -22,7 +20,6 @@ const RndFact = ({ itemsRef, isDark }) => {
         <div className="card">
           <h3>Work in progress 🚧</h3>
           <p>Se bäckasiner därmed vid ingalunda groda om, se kom strand på själv både, sjö helt sitt är björnbär. Nu kan annat stig om hans kanske söka så plats hwila, plats denna strand vi hav har del av mjuka hans, blivit vi vidsträckt mjuka helt samma bra sig enligt.</p>
-          <a href="#">Learn more</a>
         </div>
       </div>
     </div>
